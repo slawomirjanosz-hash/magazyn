148f2d383589cbdde2563f5d92ac80fde4441582
Sławomir Janosz <D:\The Bat!>
2026-01-07 00:11:29 +0100

    Add favicon for ProximaLumine logo in browser tab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Magazyn</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo_proxima_male.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
